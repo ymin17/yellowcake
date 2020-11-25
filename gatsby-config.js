8c364e93fe428a7e92352fa3d016141b25836e58
yr.min17@gmail.com <Lily@Lily-MacBook.local>
2020-11-25 21:50:40 +0900

    netlify publish

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ const postcssPresetEnv = require('postcss-preset-env')
 module.exports = {
   siteMetadata: {
     title: 'Yooyeon Kim',
-    siteUrl: 'https://yellowcake.netlify.com'
+    siteUrl: 'https://yooyeonkim.netlify.com'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
